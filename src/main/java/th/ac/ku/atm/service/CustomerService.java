cf10bc689ed2e1170d1b3ec30ae29677f3dccedb
gogogo97 <gogogo97_0_@hotmail.co.th>
2020-09-26 21:04:58 +0700

    Add CustomerRepository

--- src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -1,7 +1,9 @@
 package th.ac.ku.atm.service;
 
 import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import th.ac.ku.atm.data.CustomerRepository;
 import th.ac.ku.atm.model.Customer;
 
 import javax.annotation.PostConstruct;
@@ -10,6 +12,12 @@ import java.util.List;
 
 @Service
 public class CustomerService {
+
+    private CustomerRepository repository;
+
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
+    }
 
     private List<Customer> customerList;
 
@@ -22,7 +30,7 @@ public class CustomerService {
 //         hash pin which I don't know how to do it
         String hashPin = hash(customer.getPin());
         customer.setPin(hashPin);
-        customerList.add(customer);
+        repository.save(customer);
     }
 //    public void createCustomer(Customer customer) {
 ////         hash pin which I don't know how to do it
@@ -33,15 +41,16 @@ public class CustomerService {
 
 
     public List<Customer> getCustomers() {
-        return new ArrayList<>(this.customerList);
+        return repository.findAll();
     }
 
     public Customer findCustomer(int id) {
-        for (Customer customer : customerList) {
-            if (customer.getId() == id)
-                return customer;
+        try {
+            return repository.findById(id);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
         }
-        return null;
+
     }
 
     public Customer checkPin(Customer inputCustomer) {
